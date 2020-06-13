Fixed another bug in S/N calculation

src/pulse_find.py
@@ -262,9 +262,8 @@ def process_acf(record, time_samp, chan_width,
     record_nonzero=np.where(record_T.ravel()!=0)
 
     try:
-        # Replace masked data with a sampling of noise consistent
-        # with the rest of the observation. This is done to prevent
-        # 'ringing' in the autocorrelation function from the masked
+        # Replace masked data with mean 0, rms 1 noise
+        # This is done to prevent 'ringing' in the autocorrelation function from the masked
         # channels. One can not simply take the autocorrelation of 
         # masked data using the FFT method because the FFT algorithm
         # implicitly assumes uniformly sampled data.
@@ -272,7 +271,7 @@ def process_acf(record, time_samp, chan_width,
         median = np.median(record_ravel[record_nonzero])
         med_dev = mad(record_ravel[record_nonzero])
         record_zero = np.where(record_ravel==0)
-        normal_draw = np.random.normal(loc=median, scale=med_dev,
+        normal_draw = np.random.normal(loc=0, scale=1,
                                          size=np.shape(record_zero)[1])
 
         record_ravel[record_zero] = normal_draw
@@ -301,6 +300,10 @@ def process_acf(record, time_samp, chan_width,
 
     try:
         bandpass_corr_record = np.transpose((np.transpose(record_T) - median)/med_dev)
+        plt.imshow(bandpass_corr_record, aspect='auto')
+        plt.show()
+        plt.hist(bandpass_corr_record.ravel(), bins=30)
+        plt.show()
         freq_mean = np.mean(bandpass_corr_record, axis=1)
         where = np.where(freq_mean>= (5/np.sqrt(sub)))
         where_num = np.shape(where)[1]
@@ -318,10 +321,15 @@ def process_acf(record, time_samp, chan_width,
         bandpass_corr_record = np.zeros(np.shape(record_T))
 
 
+    #noise = np.random.normal(loc=0, scale=1, size=(64, 2048))
 
     acf_array[index, :, :] = np.ma.array(np.array(auto_corr2d_fft(
                                         bandpass_corr_record, 
                                         np.shape(record_T)[1], dtype)[0]))
+
+    # acf_array[index, :, :] = np.ma.array(np.array(auto_corr2d_fft(
+    #                                     noise, 
+    #                                     2048, dtype)[0]))
 
 
     return
@@ -1521,9 +1529,8 @@ def main():
 
             mean_array = means
 
-            N = ((2*int(time)) + 1)*((2*int(freq)) + 1) - 1#((2*time) + 1)#1 #3*((2*time) + 1)
-            stdev = 1/np.sqrt(N*num_chans*sub)
-
+            N = ((2*int(time)) + 1)*((2*int(freq)) + 1) - 1
+            stdev = np.sqrt(2)/np.sqrt(N*num_chans*sub)
 
 
             acf_norm = means/stdev